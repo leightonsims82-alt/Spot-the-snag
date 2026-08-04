5d6799e516f20ba887254686a997db5ec56d1bf0
leightonsims82-alt <leightonsims82@gmail.com>
2026-08-04 12:21:21 +0100

    Load premium landing experience

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import './styles.css';
 import './quiz.css';
 import './logo.css';
 import './premium.css';
+import './landing-upgrade.css';
+import './landing-upgrade.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
