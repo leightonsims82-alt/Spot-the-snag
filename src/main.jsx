7b8cdbbc8a793e54e47071c16bc530c1b1189938
leightonsims82-alt <leightonsims82@gmail.com>
2026-08-04 13:36:32 +0100

    Load conversion analytics and homepage teaser upgrades

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,9 @@ import './quiz.css';
 import './logo.css';
 import './premium.css';
 import './landing-upgrade.css';
+import './marketing.css';
 import './landing-upgrade.js';
+import './marketing.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
