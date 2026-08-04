1c3af8cd502e9371fbbccc3ff3048d503ab430b0
leightonsims82-alt <leightonsims82@gmail.com>
2026-08-04 21:26:44 +0100

    Switch app to Challenge the Inspector

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import ChallengeApp from './ChallengeApp.jsx';
 import './styles.css';
 import './quiz.css';
 import './logo.css';
@@ -8,12 +8,13 @@ import './premium.css';
 import './landing-upgrade.css';
 import './marketing.css';
 import './image-loader.css';
+import './challenge-upgrades.css';
 import './landing-upgrade.js';
 import './marketing.js';
 import './image-loader.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ChallengeApp />
   </React.StrictMode>,
 );
