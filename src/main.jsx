cce030c8ad861d4b783a5cdcc6a9d2a735f2bd77
leightonsims82-alt <leightonsims82@gmail.com>
2026-08-04 10:24:13 +0100

    Load premium SWSC visual styling

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx';
 import './styles.css';
 import './quiz.css';
 import './logo.css';
+import './premium.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
